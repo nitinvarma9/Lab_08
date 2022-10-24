45bd5ddfe2e677036dc2c8c431afbdcd016ada25
nitinvarma9 <82793168+nitinvarma9@users.noreply.github.com>
2022-10-23 23:13:50 -0400

    Update CheckOut.java

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -3,13 +3,13 @@ public class CheckOut {
     public static void main(String[] args) {
         Scanner pipe = new Scanner(System.in);
 
-        boolean hasMoreItems = SafeInputs.getYNConfirm(pipe, "Do you have an item to bill?");
+        boolean hasMoreItems = SafeInputs.getYNConfirm(pipe, "Do you have an item to scan?");
         double sumOfItems = 0;
         while(hasMoreItems)
         {
             double price=SafeInputs.getRangedDouble(pipe, "Enter price of your item",0.50,9.99);
             sumOfItems+=price;
-            hasMoreItems=SafeInputs.getYNConfirm(pipe, "Do you have an item to bill?");
+            hasMoreItems=SafeInputs.getYNConfirm(pipe, "Do you have an item to scan?");
         }
 
         System.out.printf("Total amount is: %.2f",sumOfItems);
